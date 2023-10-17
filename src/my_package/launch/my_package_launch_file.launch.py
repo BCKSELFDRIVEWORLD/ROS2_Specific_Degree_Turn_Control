28007687106df061659f2a7b78ccd24750a245a1
BCKSELFDRIVEWORLD <146545020+BCKSELFDRIVEWORLD@users.noreply.github.com>
2023-10-17 14:58:58 +0300

    Update my_package_launch_file.launch.py

--- src/my_package/launch/my_package_launch_file.launch.py
+++ src/my_package/launch/my_package_launch_file.launch.py
@@ -1,16 +1,16 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-            DeclareLaunchArgument(
-        'namespace',
-        default_value='SR6T2',  
-        description='rotate',
-        
         Node(
             package='my_package',
             executable='rotate',
-            output='screen'),
+            name='move_left_turtlebot3.py',
+            namespace='SR6T2',
+            output='screen',)
     ])
+
 
