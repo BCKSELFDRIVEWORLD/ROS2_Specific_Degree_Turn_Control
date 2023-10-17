93ab91b1ae8d356d7e459d2aabdbd5cd4742e075
BCKSELFDRIVEWORLD <146545020+BCKSELFDRIVEWORLD@users.noreply.github.com>
2023-10-17 14:09:32 +0300

    Update my_package_launch_file.launch.py

--- src/my_package/launch/my_package_launch_file.launch.py
+++ src/my_package/launch/my_package_launch_file.launch.py
@@ -3,11 +3,14 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+            DeclareLaunchArgument(
+        'namespace',
+        default_value='SR6T2',  
+        description='rotate',
         
         Node(
             package='my_package',
             executable='rotate',
             output='screen'),
-            remappings=[('/SR6T1/', '/SR6T2/')]  
     ])
 
