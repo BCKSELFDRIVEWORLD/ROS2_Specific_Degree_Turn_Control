e4c238153bffd312fbb612afc341d90717f76a8b
BCKSELFDRIVEWORLD <146545020+BCKSELFDRIVEWORLD@users.noreply.github.com>
2023-10-17 14:06:22 +0300

    Update my_package_launch_file.launch.py

--- src/my_package/launch/my_package_launch_file.launch.py
+++ src/my_package/launch/my_package_launch_file.launch.py
@@ -8,5 +8,6 @@ def generate_launch_description():
             package='my_package',
             executable='move_turtlebot3_left',
             output='screen'),
+            remappings=[('/SR6T1/', '/SR6T2/')]  
     ])
 
